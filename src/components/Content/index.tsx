0d3c320e102b98d3cefcbaa08cfcac27af2351b6
GazizX <162038885+GazizX@users.noreply.github.com>
2025-09-20 09:31:53 +0300

    init commit

---
+++ src/components/Content/index.tsx
@@ -0,0 +1,75 @@
+import './pic.css'
+export default function Content() {
+    return (
+        <>
+            <h1>Content</h1>
+            <div className="outer_bg">
+                <div className="inner_bg">
+                    <div className="clouds">
+                        <div className="cloud"></div>
+                        <div className="cloud"></div>
+                    </div>
+                    <div className="dragonfly">
+                        <div className="wings">
+                            <div className="wing"></div>
+                            <div className="wing"></div>
+                            <div className="wing"></div>
+                            <div className="wing"></div>
+                        </div>
+                        <div className="df_body"></div>
+                        <div className="df_head">
+                            <div className="eyes">
+                                <div className="eye"></div>
+                                <div className="eye"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="mushrooms">
+                        <div className="mushroom">
+                            <div className="ms_leg"></div>
+                            <div className="ms_head"></div>
+                        </div>
+                        <div className="mushroom">
+                            <div className="ms_leg"></div>                        
+                            <div className="ms_head"></div>
+                        </div>
+                    </div>
+                    <div className="grass-container">
+                        <div className="grass-blade"></div>
+                        <div className="grass-blade"></div>
+                        <div className="grass-blade"></div>
+                        <div className="grass-blade"></div>
+                    </div>
+                </div>
+                <ul className="dots top">
+                    <li className="dot"></li>
+                    <li className="dot"></li>
+                    <li className="dot"></li>
+                    <li className="dot"></li>
+                    <li className="dot"></li>
+                </ul>
+                <ul className="dots bottom">
+                    <li className="dot"></li>
+                    <li className="dot"></li>
+                    <li className="dot"></li>
+                    <li className="dot"></li>
+                    <li className="dot"></li>
+                </ul>
+                <ul className="dots left">
+                    <li className="dot"></li>
+                    <li className="dot"></li>
+                    <li className="dot"></li>
+                    <li className="dot"></li>
+                    <li className="dot"></li>
+                </ul>
+                <ul className="dots right">
+                    <li className="dot"></li>
+                    <li className="dot"></li>
+                    <li className="dot"></li>
+                    <li className="dot"></li>
+                    <li className="dot"></li>
+                </ul>
+            </div>        
+        </>
+    )
+}
