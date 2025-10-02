e313daaa034d65999eef8b8455ae7d36ebec6b3d
GazizX <162038885+GazizX@users.noreply.github.com>
2025-10-02 22:03:44 +0300

    main page upd

--- src/components/MainPage/index.tsx
+++ src/components/MainPage/index.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import './style.css'
+import avatarPath from '../../assets/avatar.jpg'
 export default function MainPage() {
     return (
         <>
@@ -12,6 +13,22 @@ export default function MainPage() {
                     </ul>
                 </nav>
             </header>
+
+            <main>
+                <section className="about">
+                    <img 
+                        src={avatarPath} 
+                        alt="Гоблин-аватар" 
+                        className="about_img"
+                    />
+                    <div className="about_text">
+                        <h2>Хасанов Газиз</h2>
+                        <p>🎓 Студент 3 курса</p>
+                        <p>📍 СПБПУ им. Петра Великого</p>
+                        <p>💻 Fullstack-разработчик</p>
+                    </div>
+                </section>
+            </main>
         </>
     )
 }
